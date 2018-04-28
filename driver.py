cc18999713681c2034b07738fd9fcb44e67eb4bb
Minghao Gou <gmh2015@163.com>
2018-04-28 20:07:59 +0800

    release 1

--- driver.py
+++ driver.py
@@ -14,8 +14,11 @@ def rightClick():
 
 
 if __name__ == '__main__':
+    i = 0
     s = serial.Serial(sys.argv[1])
     # s.open()
+    v = int(sys.argv[2])
+    Thresh = int(sys.argv[3])
     while(True):
         byte = s.read(1)
         # char = byte.decode('utf-8')
@@ -52,9 +55,43 @@ if __name__ == '__main__':
             ay = y0int / 32.0 * 9.8 + y1int / 256 / 32.0 * 9.8
             az = z0int / 32.0 * 9.8 + z1int / 256 / 32.0 * 9.8
             # print('x:',ax,'y:',ay,'z:',az)
-            print('x0low')
+            # print('x0high:%.8f\tx0low:%.8f'%(x0int / 32.0 * 9.8,x1int / 256 / 32.0 * 9.8))
+            if (abs(ax)> Thresh):
+                pos = win32api.GetCursorPos()
+                if(ax > 0):
+                    pos = (pos[0] - v, pos[1])
+                if(ax < 0):
+                    pos = (pos[0] + v, pos[1])
+                # if(ay > 0):
+                #     pos = (pos[0], pos[1] + v)
+                # if(ay < 0):
+                #     pos = (pos[0], pos[1] - v)
+                win32api.SetCursorPos(pos)
+            if (abs(ay)> Thresh):
+                pos = win32api.GetCursorPos()
+                # if(ax > 0):
+                #     pos = (pos[0] - v, pos[1])
+                # if(ax < 0):
+                #     pos = (pos[0] + v, pos[1])
+                if(ay > 0):
+                    pos = (pos[0], pos[1] + v)
+                if(ay < 0):
+                    pos = (pos[0], pos[1] - v)
+                win32api.SetCursorPos(pos)
 
-
-
-
-
+        elif byte == bytes([17]):
+            byte == s.read(1)
+            if byte == bytes([17]):
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+        elif byte == bytes([18]):
+            byte == s.read(1)
+            if byte == bytes([18]):
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+        elif byte == bytes([20]):
+            byte == s.read(1)
+            if byte == bytes([20]):
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
+        elif byte == bytes([21]):
+            byte == s.read(1)
+            if byte == bytes([21]):
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0)
